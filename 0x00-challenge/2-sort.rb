6a6c3faf6341a5e25c53b87ba2525162abad6049
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2023-12-23 02:37:59 -0800

    sort

---
+++ 0x00-challenge/2-sort.rb
@@ -0,0 +1,36 @@
+###
+#
+#  Sort integer arguments (ascending)
+#
+###
+
+result = []
+ARGV.each do |arg|
+  # skip if not integer
+  next if arg !~ /^-?[0-9]+$/
+
+  # convert to integer
+  i_arg = arg.to_i
+
+  # insert result at the right position
+  is_inserted = false
+  i = 0
+  l = result.size
+  while !is_inserted && i < l do
+    if result[i] < i_arg
+      i += 1
+    else
+      j = l - 1
+      while j >= i
+        result[j + 1] = result[j]
+        j -= 1
+      end
+      result[i] = i_arg
+      is_inserted = true
+      break
+    end
+  end
+  result << i_arg if !is_inserted
+end
+
+puts result
