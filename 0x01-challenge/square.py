73221ba21a584a58c5a7cdceddb7f74ebd0bbf0c
phoebe-edward <phoebe.edward.kirelloss@gmail.com>
2024-05-19 05:08:07 -0700

    square3

--- 0x01-challenge/square.py
+++ 0x01-challenge/square.py
@@ -1,11 +1,15 @@
 #!/usr/bin/python3
+""" class square"""
+
 
 class square():
+    """class square"""
 
     width = 0
     height = 0
 
     def __init__(self, *args, **kwargs):
+        """initialization"""
         for key, value in kwargs.items():
             setattr(self, key, value)
 
@@ -13,16 +17,18 @@ class square():
         """ Area of the square """
         return self.width * self.height
 
-    def PermiterOfMySquare(self):
+    def permiter_of_my_square(self):
+        """Perimeter"""
         return (self.width * 2) + (self.height * 2)
 
     def __str__(self):
+        """print"""
         return "{}/{}".format(self.width, self.height)
 
 
 if __name__ == "__main__":
-
+    """if run from this file"""
     s = square(width=12, height=9)
     print(s)
     print(s.area_of_my_square())
-    print(s.PermiterOfMySquare())
+    print(s.permiter_of_my_square())
